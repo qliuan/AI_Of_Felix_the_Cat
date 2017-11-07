88226fb0165ad5506019a59316a04c75d316a9cd
Ziyao ZHANG <zzhangbk@connect.ust.hk>
2017-11-08 01:00:24 +0800

    Add files via upload

--- felix.py
+++ felix.py
@@ -1,5 +1,40 @@
 import random
+import ast #ast.literal_eval
 
+MODE = 0
+# 0: manual
+# 1: random_agent
+PRINT_MODE = "tior"
+# a: All / Always
+# t: Title
+# i: Information
+# o:Output
+# r: Result
+# d: Debug
+
+def printm (text, text_type):
+     global PRINT_MODE
+     print_mode = PRINT_MODE
+     if (text_type == "a") or ("a" in text_type) or (text_type in print_mode):
+          print(text)
+
+def add_frame (title, frame, padding):
+     title_length = len(title)
+     frame_length = title_length + 2 * padding
+     framed_title = "\n"
+     for _ in range(frame_length):
+          framed_title = framed_title + frame
+     framed_title = framed_title + "\n"
+     for _ in range(padding):
+          framed_title = framed_title + "x"
+     framed_title = framed_title + title
+     for _ in range(padding):
+          framed_title = framed_title + "x"
+     framed_title = framed_title + "\n"
+     for _ in range(frame_length):
+          framed_title = framed_title + frame
+     return framed_title
+               
 ### Console Display Set-up ###
 
 fig_unrevealed = "*"
@@ -44,7 +79,6 @@ def compute_series_score (series, deck_value):
 
      return score
 
-
 default_deck = {} # dictionary <string, bool>
 for key in list(default_deck_value.keys()):
      default_deck[key] = True
@@ -58,7 +92,7 @@ def show_deck (deck):
                deck_list.append(key)
      return deck_list
 
-total_round = 10
+total_round = 10 #
 default_score = 0
 default_token = 15
 max_bid = 2
@@ -78,7 +112,7 @@ def  rewards_info (skip_rewards, reward_pointer):
 
 player_template = {
      "deck": {}, # dictionary <string, bool>
-     "show_deck_public": {},
+     "show_deck_public": [],
      "score": default_score,
      "token": default_token,
      "bid": 0,
@@ -115,81 +149,100 @@ def player_info_game_over (player):
 
 ### Agent ###
 
-agent_input = {
-     "total_round": total_round,
-     "default_deck_value": default_deck_value,
-     "max_bid": max_bid,
+agent_input_template = {
+     "my_index": -1, # index of current player
+     "rule_total_round": total_round,
+     "rule_default_deck_value": default_deck_value, # dictionary <string, int>
+     "rule_max_bid": max_bid,
      "round": -1,
      "starting_player_index": -1,
+     "stage": 0, # 1 for Selling Stage, 2 for Bidding Stage
      "players_public": [],
-     "my_deck": [],
+     "my_deck": {},
      "central_series_public": [],
-     "skip_rewards": skip_rewards,
+     "rule_skip_rewards": skip_rewards,
      "reward_pointer": -1,
      "current_highest_bid": -1
 }
 
-# output card_to_sell (string)
-def selling_handler_manual (player, agent_input):
-     # possible conditions
-     card_to_sell = input("Please choose a card to sell: ")
-     invalid_card = (card_to_sell not in player["deck"])
-     played_card = (not invalid_card) and (player["deck"][card_to_sell] == False)
-     while (invalid_card or played_card):
-          print("You don't have " + card_to_sell)
-          card_to_sell = input("Please choose a card to sell: ")
-          # recompute possible conditions
-          invalid_card = (card_to_sell not in player["deck"])
-          played_card = (not invalid_card) and (player["deck"][card_to_sell] == False)
-     return card_to_sell
+agent_output_template = {
+     "card_to_sell": "INITIAL",
+     "bid_to_add": -1
+}
 
-# output bid_to_add (int)
-def bidding_handler_manual (player, agent_input):
-     bid_to_add = input("Please decide the bid to add (input 0 to skip): ")
-     # possible conditions
-     numeric = bid_to_add.isnumeric()
-     skip = numeric and (int(bid_to_add) == 0)
-     negative_input = numeric and (int(bid_to_add) < 0)
-     weak_bid = numeric and (player["bid"] + int(bid_to_add) <= agent_input["current_highest_bid"]) and (not skip)
-     insufficient_token = numeric and (int(bid_to_add) > current_player["token"])
-     while ((not numeric) or negative_input or weak_bid or insufficient_token):
-          if (not numeric):
-               bid_to_add = input("Please input a number:")
-          elif (negative_input):
-               bid_to_add = input("Please input a non-negative number:")
-          elif (weak_bid):
-               print("Your bid (" + str(current_player["bid"] + int(bid_to_add)) + \
-                     ") cannot beat the highest bid (" + str(current_highest_bid) + ")")
-               bid_to_add = input("Please decide the bid to add (input 0 to skip): ")
-          else: # insufficient_token
-               print("You don't have enough tokens (" + str(current_player["token"]) + ")!")
-               bid_to_add = input("Please decide the bid to add (input 0 to skip): ")
-           # recompute possible conditions    
+game_result = {
+     "winner": -1,
+     "total_scores": []
+}
+
+def handler_manual (agent_input, agent_output):
+     current_player = agent_input["players_public"][agent_input["my_index"]]
+     stage = agent_input["stage"]
+     if (stage == 1):
+          # possible conditions
+          card_to_sell = input("Please choose a card to sell: ")
+          invalid_card = (card_to_sell not in agent_input["my_deck"])
+          played_card = (not invalid_card) and (agent_input["my_deck"][card_to_sell] == False)
+          while (invalid_card or played_card):
+               print("You don't have " + card_to_sell)
+               card_to_sell = input("Please choose a card to sell: ")
+               # recompute possible conditions
+               invalid_card = (card_to_sell not in agent_input["my_deck"])
+               played_card = (not invalid_card) and (agent_input["my_deck"][card_to_sell] == False)
+          agent_output["card_to_sell"] = card_to_sell
+     else:
+          bid_to_add = input("Please decide the bid to add (input 0 to skip): ")
+          # possible conditions
           numeric = bid_to_add.isnumeric()
           skip = numeric and (int(bid_to_add) == 0)
           negative_input = numeric and (int(bid_to_add) < 0)
-          weak_bid = numeric and (player["bid"] + int(bid_to_add) <= agent_input["current_highest_bid"]) and (not skip)
+          weak_bid = numeric and (current_player["bid"] + int(bid_to_add) <= agent_input["current_highest_bid"]) and (not skip)
           insufficient_token = numeric and (int(bid_to_add) > current_player["token"])
-     return int(bid_to_add)
+          while ((not numeric) or negative_input or weak_bid or insufficient_token):
+               if (not numeric):
+                    bid_to_add = input("Please input a number:")
+               elif (negative_input):
+                    bid_to_add = input("Please input a non-negative number:")
+               elif (weak_bid):
+                    print("Your bid (" + str(current_player["bid"] + int(bid_to_add)) + \
+                          ") cannot beat the highest bid (" + str(current_highest_bid) + ")")
+                    bid_to_add = input("Please decide the bid to add (input 0 to skip): ")
+               else: # insufficient_token
+                    print("You don't have enough tokens (" + str(current_player["token"]) + ")!")
+                    bid_to_add = input("Please decide the bid to add (input 0 to skip): ")
+                # recompute possible conditions    
+               numeric = bid_to_add.isnumeric()
+               skip = numeric and (int(bid_to_add) == 0)
+               negative_input = numeric and (int(bid_to_add) < 0)
+               weak_bid = numeric and (current_player["bid"] + int(bid_to_add) <= agent_input["current_highest_bid"]) and (not skip)
+               insufficient_token = numeric and (int(bid_to_add) > current_player["token"])
+          agent_output["bid_to_add"]  = int(bid_to_add)
 
-def selling_handler_random_agent (player, agent_input):
-     card_to_sell = random.choice(show_deck(player["deck"]))
-     print("RANDOM AGENT sells " + card_to_sell)
-     #hold = input("Press any key to continue...")
-     return card_to_sell
-
-def bidding_handler_random_agent (player, agent_input):
-     max_bid_to_add = min(
-          [agent_input["current_highest_bid"] - player["bid"] + 1, current_player["token"], agent_input["max_bid"]]
-     )
-     if (max_bid_to_add == 0):
-          bid_to_add = 0
+def handler_random_agent (agent_input, agent_output):
+     printm("INPUT DEBUG: ", "d")
+     printm(agent_input, "d")
+     current_player = agent_input["players_public"][agent_input["my_index"]]
+     stage = agent_input["stage"]
+     if (stage == 1):
+          card_to_sell = random.choice(show_deck(agent_input["my_deck"]))
+          printm("RANDOM AGENT sells " + card_to_sell, "o")
+          agent_output["card_to_sell"] = card_to_sell
      else:
-          bid_to_add = random.randrange(max_bid_to_add + 1)
-     print("RANDOM AGENT adds " + str(bid_to_add))
+          max_bid_to_add = min(
+               [agent_input["current_highest_bid"] - current_player["bid"] + 1, \
+                current_player["token"], \
+                agent_input["rule_max_bid"]]
+          )
+          if (max_bid_to_add == 0):
+               bid_to_add = 0
+          else:
+               bid_to_add = random.randrange(max_bid_to_add + 1)
+          printm("RANDOM AGENT adds " + str(bid_to_add), "o")
+          agent_output["bid_to_add"]  = int(bid_to_add)
      #hold = input("Press any key to continue...")
-     return bid_to_add
-
+     printm("OUTPUT DEBUG: ", "d")
+     printm(agent_output, "d")
+     
 ### Game ###
 
 current_round = 0
@@ -216,15 +269,17 @@ def show_series (series, revealed_length):
 
 ### Game Start ###
 
-print("\n### GAME START ###")
+printm("\n### GAME START ###", "t")
 
 while (current_round < total_round):
 
      current_round += 1
 
-     print("\n========")
-     print(" Round " + str(current_round))
-     print("========")
+     agent_input = agent_input_template.copy()
+     agent_output = agent_output_template.copy()
+
+     printm(add_frame("Round "+str(current_round), "=", 2), "t")
+     
      agent_input["round"] = current_round
      agent_input["starting_player_index"] = starting_player_index
 
@@ -248,9 +303,10 @@ while (current_round < total_round):
 
      # Selling Stage
 
-     print("\n-----------------")
-     print(" Selling Stage")
-     print("-----------------")
+     printm("\n-----------------", "t")
+     printm(" Selling Stage", "t")
+     printm("-----------------", "t")
+     agent_input["stage"] = 1
 
      for i in range(num_of_player):
 
@@ -258,7 +314,8 @@ while (current_round < total_round):
           current_player = players[current_player_index]
           current_deck = current_player["deck"]
 
-          print("\n<Player " + str(current_player_index) + "'s Turn>\n")
+          printm("\n<Player " + str(current_player_index) + "'s Turn>\n", "t")
+          agent_input["my_index"] = current_player_index
           
           # information (excluding bid) of all players
           agent_input["players_public"] = []
@@ -267,24 +324,28 @@ while (current_round < total_round):
                     placeholder = fig_arrow
                else:
                     placeholder = ""
-               print("Player " + str(i) + placeholder + "\n" + player_info(players[i]) )
+               printm("Player " + str(i) + placeholder + "\n" + player_info(players[i]), "i")
                agent_input["players_public"].append(players[i].copy())
                del agent_input["players_public"][i]["deck"]
         
-          print("\n" + str(show_deck(current_deck)))
-          agent_input["my_deck"] = show_deck(current_deck)
-
-          ###card_to_sell = selling_handler_manual (current_player, agent_input)
-          card_to_sell = selling_handler_random_agent (current_player, agent_input)
-               
+          printm("\n" + str(show_deck(current_deck)), "i")
+          agent_input["my_deck"] = current_deck
+          
+          if (MODE) == 1:
+               handler_random_agent(agent_input, agent_output)
+          else:
+               handler_manual(agent_input, agent_output)
+          card_to_sell = agent_output["card_to_sell"]
+          
           current_deck[card_to_sell] = False
           central_series.append(card_to_sell)
 
      # Bidding Stage
 
-     print("\n-----------------")
-     print("Bidding Stage")
-     print("-----------------")
+     printm("\n-----------------", "t")
+     printm("Bidding Stage", "t")
+     printm("-----------------", "t")
+     agent_input["stage"] = 2
 
      current_player_index = starting_player_index - 1
 
@@ -299,7 +360,9 @@ while (current_round < total_round):
           if (current_player["skipped"]):
                continue
 
-          print("\n<Player " + str(current_player_index) + "'s Turn>\n")
+          printm("\n<Player " + str(current_player_index) + "'s Turn>\n", "t")
+          agent_input["my_index"] = current_player_index
+          agent_input["my_deck"] = current_deck
           
           # information (including bid) of all players
           agent_input["players_public"] = []
@@ -308,14 +371,14 @@ while (current_round < total_round):
                     placeholder = fig_arrow
                else:
                     placeholder = ""
-               print("Player " + str(i) + placeholder + "\n" + player_info_bid(players[i]))
+               printm("Player " + str(i) + placeholder + "\n" + player_info_bid(players[i]), "i")
                agent_input["players_public"].append(players[i].copy())
                del agent_input["players_public"][i]["deck"]
           
-          print("\n" + str(show_series(central_series, central_series_revealed_length)))
+          printm("\n" + str(show_series(central_series, central_series_revealed_length)), "i")
           agent_input["central_series_public"] = show_series(central_series, central_series_revealed_length)
-          print("Starting Player: " + str(starting_player_index))
-          print(rewards_info(skip_rewards, reward_pointer))
+          printm("Starting Player: " + str(starting_player_index), "i")
+          printm(rewards_info(skip_rewards, reward_pointer), "i")
           agent_input["reward_pointer"] = reward_pointer
           agent_input["current_highest_bid"] = current_highest_bid
 
@@ -325,27 +388,30 @@ while (current_round < total_round):
           if (current_player["token"] <= current_highest_bid - current_player["bid"]):
                if (fleeing):
                     blanking = True
-               print("You are forced to skip since you don't have enough tokens to win the bid.")
+               printm("You are forced to skip since you don't have enough tokens to win the bid.", "o")
                skip = True
 
           # not forced to skip  
           else:
                if (fleeing):
-                    print("All other players fleed. Now you are the only bidder.")
-                    
-               ###bid_to_add = bidding_handler_manual (current_player, agent_input)
-               bid_to_add = bidding_handler_random_agent (current_player, agent_input)
+                    printm("All other players fleed. Now you are the only bidder.", "o")
+
+               if (MODE == 1):
+                    handler_random_agent(agent_input, agent_output)
+               else:
+                    handler_manual(agent_input, agent_output)
+               bid_to_add = agent_output["bid_to_add"]
                
                skip = (bid_to_add == 0)
                                    
                if  (skip):
-                    print("You chose to skip.")
+                    printm("You choose to skip.", "o")
                else:
                     current_player["token"] -= bid_to_add
                     current_player["bid"] += bid_to_add
                     current_highest_bid = current_player["bid"]
                     current_highest_bidder_index = current_player_index
-                    print("You added your bid to " + str(current_player["bid"]))
+                    printm("You add your bid to " + str(current_player["bid"]), "o")
 
           if (skip):
                current_player["skipped"] = True
@@ -359,7 +425,7 @@ while (current_round < total_round):
                     placeholder = " tokens."
                else:
                     placeholder = " token."
-               print("You receive a skip reward of " + str(reward) + placeholder)
+               printm("You receive a skip reward of " + str(reward) + placeholder, "o")
                reward_pointer += 1
                central_series_revealed_length += 1
                
@@ -375,10 +441,10 @@ while (current_round < total_round):
           if (fleeing_loop_terminator):
                break
 
-     print("\n" + str(show_series(central_series, central_series_revealed_length)))
+     printm("\n" + str(show_series(central_series, central_series_revealed_length)), "i")
 
      if (blanking):
-          print("All players skipped.")
+          printm("All players choose to skip.", "r")
      else:
           if (fleeing): # fleeing but not blanking
                bid_winner_index = current_player_index
@@ -396,32 +462,36 @@ while (current_round < total_round):
           else:
                placeholder = " token."
 
-          print("Player " + str(bid_winner_index) + " win the bid of score " +\
-                str(score) + " at the cost of " + str(cost) + placeholder)
+          printm("Player " + str(bid_winner_index) + " win the bid of score " +\
+                str(score) + " at the cost of " + str(cost) + placeholder, "r")
 
           starting_player_index = bid_winner_index
 
 # Game Over #
 
-print("\n### GAME OVER ###\n")
+printm("\n### GAME OVER ###\n", "t")
 
 total_scores = []
 
 for i in range(num_of_player):
      total_scores.append(players[i]["score"] + players[i]["token"])
-     print("Player " + str(i) + "\n" + player_info_game_over(players[i]) )
+     printm("Player " + str(i) + "\n" + player_info_game_over(players[i]), "r")
 
 winner_index = total_scores.index(max(total_scores))
 
-print("\n--------------")
-print("Total Score")
-print("--------------")
+# for agent
+game_result["winner"] = winner_index
+game_result["total_scores"] = total_scores
+
+printm("\n--------------", "r")
+printm("Total Score", "r")
+printm("--------------", "r")
 
 for i in range(num_of_player):
      if (i == winner_index):
           placeholder = fig_arrow
      else:
           placeholder = ""
-     print("Player " + str(i) + " " + str(total_scores[i]) + placeholder)
+     printm("Player " + str(i) + " " + str(total_scores[i]) + placeholder, "r")
 
-print("\nThe winner is Player " + str(winner_index) + "!")
+printm("\nThe winner is Player " + str(winner_index) + "!", "a")
